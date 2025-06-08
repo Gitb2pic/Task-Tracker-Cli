f2b28f110adbd27c1b73b8c040673a846dc0ac81
Gitb2pic <alexmpamidev@gmail.com>
2025-06-08 05:22:54 +0100

    Add argument parser initialization for Task Management CLI

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
 import argparse
 from src.controllers.config import create_task, listing_all_tasks, update_task, delete_task, print_task_statuses, change_task_status
 
+parser = argparse.ArgumentParser(description="Task Management CLI")
